1e6bc32e5c340fbd13c9ad07ba4b3e1371fb68f6
GreenMoCh <moadchabaa0@gmail.com>
2023-10-12 09:34:20 -0700

    Create BaseModel from dict

---
+++ models/base_model.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""This module defines a base class for all models"""
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    """Class that defines common attrinutes and methods"""
+
+    def __init__(self, *args, **kwargs):
+        """Initializes the BaseModel instance with a unique ID and timestamps"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Return a str representation of an object"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """Update the public instance attribute 'updated_at' with the current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Return a dict contaning all keys/values of the instance"""
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return (obj_dict)
